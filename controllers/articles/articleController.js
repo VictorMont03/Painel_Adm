82d5b103a7972b49e64fa5c6dc4364beee2f608a
VictorMont <victormonteir2@gmail.com>
2021-09-01 11:26:20 -0300

    Paginação e edição de artigos

--- controllers/articles/articleController.js
+++ controllers/articles/articleController.js
@@ -19,6 +19,37 @@ router.get("/admin/articles/new", (req, res) => {
     
 });
 
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id;
+    if(isNaN(id)){
+        res.redirect("/admin/articles");
+    }
+    Article.findByPk(id).then((article) => {
+        if(article != undefined){
+            Category.findAll().then((categories) => {
+                res.render("admin/articles/edit", {article: article, categories: categories});
+             });  
+        }else{
+            res.redirect("/admin/articles");
+        }
+    }).catch((err) => {
+        res.redirect("/admin/articles");
+        console.log(err);
+    })
+})
+
+router.post('/articles/update', (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var categoryId = req.body.categoryId;
+    Article.update({title: title, slug: slugify(title), body: body, categoryId: categoryId}, {where: {id: id}}).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
 router.post("/articles/delete", (req, res) => {
     var id = req.body.id;
     if(id != undefined){
@@ -53,6 +84,42 @@ router.post("/articles/save", (req, res) => {
         res.rendirect("/admin/articles/new");
     }
 })
+
+router.get('/articles/page/:number', (req, res) => {
+    var page = parseInt(req.params.number);
+    var offset = 0;
+    if(!isNaN(page) || page <= 1){
+        offset = 0;
+    }
+    if(page > 1){
+        offset = ((page - 1)*4);
+    }
+    Article.findAndCountAll({
+        limit: 4,
+        order: [
+            ['id','DESC']
+        ], 
+        offset: offset
+    }).then((articles) => {
+        var next;
+        if((offset + 4) >= articles.count){
+            next = false;
+        }else{
+            next = true;
+        }
+        var results = {
+            next: next,
+            page: page,
+            articles: articles
+        }
+
+        //res.json(results);
+
+        Category.findAll().then((categories) => {
+            res.render("admin/articles/page", {results: results, categories: categories});
+         });  
+    })
+});
 
 
 module.exports = router;
